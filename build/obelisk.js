205ac01f0f5596304b292840b6cc17fd562197cc
nosir <toorison@gmail.com>
2014-03-04 23:12:56 +1100

    Add canvas tool class

--- build/obelisk.js
+++ build/obelisk.js
@@ -1602,6 +1602,41 @@
 /*global obelisk:true*/
 
 /*
+ * CanvasTool
+ */
+
+(function (obelisk) {
+    "use strict";
+
+    var CanvasTool, p;
+
+    CanvasTool = function () {
+        throw new Error('CanvasTool is a static Class, cannot be instanced.');
+    };
+    p = CanvasTool;
+
+    // public methods
+    p.getPixel = function (imageData, x, y) {
+        var data, index, r, g, b;
+        data = imageData.data;
+        index = (y * imageData.width + x) * 4;
+        r = data[index];
+        g = data[index + 1];
+        b = data[index + 2];
+
+        return ((r << 16) | (g << 8) | b);
+    }
+
+    p.toString = function () {
+        return "[CanvasTool]";
+    };
+
+    obelisk.CanvasTool = CanvasTool;
+}(obelisk));
+
+/*global obelisk:true*/
+
+/*
  * ColorGeom
  */
 
---
+++ src/utils/CanvasTool.js
@@ -0,0 +1,34 @@
+/*global obelisk:true*/
+
+/*
+ * CanvasTool
+ */
+
+(function (obelisk) {
+    "use strict";
+
+    var CanvasTool, p;
+
+    CanvasTool = function () {
+        throw new Error('CanvasTool is a static Class, cannot be instanced.');
+    };
+    p = CanvasTool;
+
+    // public methods
+    p.getPixel = function (imageData, x, y) {
+        var data, index, r, g, b;
+        data = imageData.data;
+        index = (y * imageData.width + x) * 4;
+        r = data[index];
+        g = data[index + 1];
+        b = data[index + 2];
+
+        return ((r << 16) | (g << 8) | b);
+    }
+
+    p.toString = function () {
+        return "[CanvasTool]";
+    };
+
+    obelisk.CanvasTool = CanvasTool;
+}(obelisk));
